7317891af336ddf0be124850c36c4668e38a8aff
William Laderer <wtladerer@gmail.com>
2022-10-20 22:02:40 -0700

    its working!

--- Zhum/zhum/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Zhum in a project::
-
-    import zhum
--- Zhum/zhum/zhum/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Zhum."""
-
-__author__ = """William Laderer"""
-__email__ = 'wtladerer@gmail.com'
-__version__ = '0.1.0'
